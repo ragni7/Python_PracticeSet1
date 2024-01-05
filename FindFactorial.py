c472040df42d6bbad5e6146766b5659e8cf40e75
ragni7 <152313149+ragni7@users.noreply.github.com>
2024-01-06 00:40:35 +0530

    Create FindFactorial.py

---
+++ FindFactorial.py
@@ -0,0 +1,9 @@
+import math
+
+def factorial(factofnum):
+    fact=(math.factorial(factofnum))
+    return fact
+
+factresult = int(factorial(5))
+#num = int(input("Enter the number:"))
+print("Factorial is", factresult)
