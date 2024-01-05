346c3758993532962cee8595d0091a37c04a89a0
ragni7 <152313149+ragni7@users.noreply.github.com>
2024-01-06 00:41:29 +0530

    Create CountStringWords.py

---
+++ CountStringWords.py
@@ -0,0 +1,8 @@
+def count_words():
+    countOfWords = len("It should count the words in a sentence".split())
+    return countOfWords
+    print(countOfWords)
+
+
+NumberOfWords = count_words()
+print(NumberOfWords)
